d2f574efd59ab75ab61320e57d177bb54c470762
Yohann <pardes.yoyo@gmail.com>
2024-11-30 21:29:00 +0200

    simllified the Paris function for it to be more readable. Also improved the simple graph example for it to be more understandable

--- examples/simple_graph.py
+++ examples/simple_graph.py
@@ -4,6 +4,8 @@
 Demo of the Paris algorithm on a simple graph
 =============================================
 """
+import math
+
 print(__doc__)
 
 from community import best_partition
@@ -13,61 +15,55 @@ from python_paris.homogeneous_cut_slicer import *
 from python_paris.heterogeneous_cut_slicer import *
 from python_paris.distance_slicer import *
 
+import random
+
+
+def generate_graph_with_euclidean_weights(num_nodes, additional_edges=0):
+    """
+    Generate a graph with nodes and edges where weights are based on Euclidean distances.
+
+    Parameters:
+        num_nodes (int): The number of nodes in the graph.
+        additional_edges (int): The number of extra edges to add randomly beyond the minimum.
+
+    Returns:
+        tuple: A list of edges (u, v, weight) and a dictionary of node positions.
+    """
+    # Generate random 2D positions for each node
+    node_positions = {i: (random.uniform(0, 100), random.uniform(0, 100)) for i in range(num_nodes)}
+
+    # Ensure all nodes are connected by forming a linear chain
+    edges = []
+    for i in range(num_nodes - 1):
+        u, v = i, i + 1
+        pos_u, pos_v = node_positions[u], node_positions[v]
+        weight = math.sqrt((pos_u[0] - pos_v[0]) ** 2 + (pos_u[1] - pos_v[1]) ** 2)
+        edges.append((u, v, weight))
+
+    # Add additional random edges
+    existing_edges = set((u, v) for u, v, _ in edges)
+    for _ in range(additional_edges):
+        while True:
+            u = random.randint(0, num_nodes - 1)
+            v = random.randint(0, num_nodes - 1)
+            if u != v and (u, v) not in existing_edges and (v, u) not in existing_edges:
+                pos_u, pos_v = node_positions[u], node_positions[v]
+                weight = math.sqrt((pos_u[0] - pos_v[0]) ** 2 + (pos_u[1] - pos_v[1]) ** 2)
+                edges.append((u, v, weight))
+                existing_edges.add((u, v))
+                break
+
+    return edges, node_positions
+
+
 # ############################################################################################
 # Generate the graph
+N_NODES = 30
+ADDITIONAL_EDGES = int(N_NODES * 0.4)
 graph = nx.Graph()
-graph.add_nodes_from([0, 1, 2, 3, 4, 5, 6 ,7, 8, 9, 10, 11, 12, 13, 14])
-graph.add_weighted_edges_from([(0, 1, 1), (0, 2, 1), (1, 2, 1), (2, 3, 1),
-                      (3, 4, 1), (3, 5, 1), (4, 5, 1), (5, 6, 1), (5, 7, 1), (6, 7, 1), (7, 8, 1), (7, 9, 1), (8, 9, 1), (9, 10, 1), (9, 11, 1), (10, 11, 1), (11, 12, 1), (11, 13, 1), (12, 13, 1), (13, 14, 1)])
-
-
-# import networkx as nx
-# import random
-
-# # Create a new graph
-# complex_graph = nx.Graph()
-
-# # Add nodes
-# num_nodes = 15  # Adjust the number of nodes for complexity
-# complex_graph.add_nodes_from(range(num_nodes))
-
-# # Add random edges
-# num_edges = 0  # Adjust the number of edges for complexity
-# for _ in range(num_edges):
-#     u, v = random.sample(range(num_nodes), 2)  # Randomly select two distinct nodes
-#     weight = random.randint(1, 10)  # Assign a random weight to the edge
-#     complex_graph.add_edge(u, v, weight=weight)
-
-# # Add some cliques (fully connected subgraphs)
-# clique_nodes = [random.sample(range(num_nodes), 5) for _ in range(3)]  # Create 3 cliques of size 5
-# for clique in clique_nodes:
-#     complex_graph.add_edges_from([(clique[i], clique[j]) for i in range(len(clique)) for j in range(i + 1, len(clique))])
-
-# # Add a star structure (one central node connected to many others)
-# central_node = random.choice(range(num_nodes))
-# leaf_nodes = random.sample(range(num_nodes), 8)  # Select 8 nodes for the star
-# complex_graph.add_edges_from([(central_node, leaf) for leaf in leaf_nodes])
-
-# # Add a line structure (a path connecting a sequence of nodes)
-# line_nodes = random.sample(range(num_nodes), 10)  # Select 10 nodes for the line
-# for i in range(len(line_nodes) - 1):
-#     complex_graph.add_edge(line_nodes[i], line_nodes[i + 1])
-
-# # Add a cycle
-# cycle_nodes = random.sample(range(num_nodes), 6)  # Select 6 nodes for the cycle
-# for i in range(len(cycle_nodes)):
-#     complex_graph.add_edge(cycle_nodes[i], cycle_nodes[(i + 1) % len(cycle_nodes)])
-
-# #make sure all edges have a weight
-# graph = complex_graph
-# for u, v in graph.edges:
-#     if 'weight' not in graph[u][v]:
-#         graph[u][v]['weight'] = 1  # Assign a default weight
-
-
-# print("Complex graph created with:")
-# print(f"- {complex_graph.number_of_nodes()} nodes")
-# print(f"- {complex_graph.number_of_edges()} edges")
+graph.add_nodes_from([i for i in range(0, N_NODES)])
+edges, positions = generate_graph_with_euclidean_weights(N_NODES, ADDITIONAL_EDGES)
+graph.add_weighted_edges_from(edges)
 
 
 # ############################################################################################
@@ -95,32 +91,12 @@ print("Plot the result\n")
 import matplotlib.pyplot as plt
 
 colors = ['b', 'g', 'r', 'c', 'm', 'y']
-pos = nx.fruchterman_reingold_layout(graph)
+pos = nx.fruchterman_reingold_layout(graph, pos=positions, iterations=500, dim = 2)
 
-plt.subplot(2, 2, 1)
+plt.plot()
 plt.title('Best cluster')
 plt.axis('off')
 nx.draw_networkx_edges(graph, pos)
 nodes = nx.draw_networkx_nodes(graph, pos, node_color='k')
 nx.draw_networkx_nodes(graph, pos, nodelist=best_cluster, node_color=colors[0])
-
-plt.subplot(2, 2, 2)
-plt.title('Best homogeneous clustering')
-plt.axis('off')
-nx.draw_networkx_edges(graph, pos)
-for l in range(min(len(colors), len(best_homogeneous_clustering))):
-    nx.draw_networkx_nodes(graph, pos, nodelist=best_homogeneous_clustering[l], node_color=colors[l])
-
-plt.subplot(2, 2, 3)
-plt.title('Best heterogeneous clustering')
-plt.axis('off')
-nx.draw_networkx_edges(graph, pos)
-for l in range(min(len(colors), len(best_heterogeneous_clustering))):
-    nx.draw_networkx_nodes(graph, pos, nodelist=best_heterogeneous_clustering[l], node_color=colors[l])
-
-plt.subplot(2, 2, 4)
-plt.title('Best distance')
-plt.axis('off')
-nx.draw_networkx_edges(graph, pos)
-nx.draw_networkx_nodes(graph, pos, node_color=[colors[best_louvain_clustering[node]] for node in graph])
 plt.show()
--- python_paris/paris.py
+++ python_paris/paris.py
@@ -1,357 +1,205 @@
-# import numpy as np
-# import networkx as nx
-
-
-# def paris(graph):
-#     """
-#      Given a graph, compute the paris hierarchy.
-
-#      Parameters
-#      ----------
-#      dendrogram: networkx.graph
-#          A graph with weighted edges.
-
-#      Returns
-#      -------
-#      dendrogram: numpy.array
-#          The paris hierachical clustering is represneted by the dendrogram. Each line of the dendrogram contains the
-#          merged nodes, the distance between merged nodes and the number of nodes in the new cluster.
-
-#      References
-#      ----------
-#      -
-#      """
-#     graph_copy = graph.copy()
-#     nodes = list(graph_copy.nodes())
-#     n_nodes = len(nodes)
-#     graph_copy = nx.convert_node_labels_to_integers(graph_copy)
-#     if nx.get_edge_attributes(graph_copy, 'weight') == {}:
-#         for u, v in graph_copy.edges():
-#             graph_copy.add_edge(u, v, weight=1)
-
-#     w = {u: 0 for u in range(n_nodes)}
-#     wtot = 0
-#     for (u, v) in graph_copy.edges():
-#         weight = graph_copy[u][v]['weight']
-#         w[u] += weight
-#         w[v] += weight
-#         wtot += 2 * weight
-#     s = {u: 1 for u in range(n_nodes)}
-#     cc = []
-#     dendrogram = []
-#     u = n_nodes
-
-#     while n_nodes > 0:
-#         chain = [list(graph_copy.nodes())[0]]
-#         while chain != []:
-#             a = chain.pop()
-#             d_min = float("inf")
-#             b = -1
-#             neighbors_a = list(graph_copy.neighbors(a))
-#             for v in neighbors_a:
-#                 if v != a:
-#                     d = w[v] * w[a] / float(graph_copy[a][v]['weight']) / float(wtot)
-#                     if d < d_min:
-#                         b = v
-#                         d_min = d
-#                     elif d == d_min:
-#                         b = min(b, v)
-#             d = d_min
-#             if chain != []:
-#                 c = chain.pop()
-#                 if b == c:
-#                     dendrogram.append([a, b, d, s[a] + s[b]])
-#                     graph_copy.add_node(u)
-#                     neighbors_a = list(graph_copy.neighbors(a))
-#                     neighbors_b = list(graph_copy.neighbors(b))
-#                     for v in neighbors_a:
-#                         graph_copy.add_edge(u, v, weight=graph_copy[a][v]['weight'])
-#                     for v in neighbors_b:
-#                         if graph_copy.has_edge(u, v):
-#                             graph_copy[u][v]['weight'] += graph_copy[b][v]['weight']
-#                         else:
-#                             graph_copy.add_edge(u, v, weight=graph_copy[b][v]['weight'])
-#                     graph_copy.remove_node(a)
-#                     graph_copy.remove_node(b)
-#                     n_nodes -= 1
-#                     w[u] = w.pop(a) + w.pop(b)
-#                     s[u] = s.pop(a) + s.pop(b)
-#                     u += 1
-#                 else:
-#                     chain.append(c)
-#                     chain.append(a)
-#                     chain.append(b)
-#             elif b >= 0:
-#                 chain.append(a)
-#                 chain.append(b)
-#             else:
-#                 cc.append((a, s[a]))
-#                 graph_copy.remove_node(a)
-#                 w.pop(a)
-#                 s.pop(a)
-#                 n_nodes -= 1
-
-#     a, s = cc.pop()
-#     for b, t in cc:
-#         s += t
-#         dendrogram.append([a, b, float("inf"), s])
-#         a = u
-#         u += 1
-
-#     return reorder_dendrogram(np.array(dendrogram))
-
-
-# def reorder_dendrogram(dendrogram):
-#     """
-#      Given a graph, compute the paris hierarchy
-
-#      Parameters
-#      ----------
-#      dendrogram: numpy.array
-#          Each line of the dendrogram contains the merged nodes, the distance between merged nodes and the number of
-#          nodes in the new cluster. The lines are not sorted with respect to increasing distances.
-
-#      Returns
-#      -------
-#      dendrogram: numpy.array
-#          Each line of the dendrogram contains the merged nodes, the distance between merged nodes and the number of
-#          nodes in the new cluster. The lines are sorted with respect to increasing distances.
-
-#      References
-#      ----------
-#      -
-#      """
-#     n = np.shape(dendrogram)[0] + 1
-#     order = np.zeros((2, n - 1), float)
-#     order[0] = range(n - 1)
-#     order[1] = np.array(dendrogram)[:, 2]
-#     index = np.lexsort(order)
-#     n_index = {i: i for i in range(n)}
-#     n_index.update({n + index[t]: n + t for t in range(n - 1)})
-#     return np.array([[n_index[int(dendrogram[t][0])], n_index[int(dendrogram[t][1])], dendrogram[t][2], dendrogram[t][3]] for t in range(n - 1)])[index, :]
-
-
-
-
-
 import numpy as np
 import networkx as nx
 
+
 def paris(graph):
     """
-    Computes the Paris hierarchical clustering for a given graph.
+    Given a graph, compute the Paris hierarchy.
 
     Parameters
     ----------
-    graph : networkx.Graph
-        Input graph with weighted edges.
+    graph: networkx.Graph
+        A graph with weighted edges.
 
     Returns
     -------
-    dendrogram : numpy.array
-        Hierarchical clustering represented as a dendrogram. Each row contains:
-        [merged node 1, merged node 2, distance, size of new cluster].
+    dendrogram: numpy.array
+        The Paris hierarchical clustering is represented by the dendrogram. Each line of the dendrogram contains:
+        - The merged nodes
+        - The distance between merged nodes
+        - The number of nodes in the new cluster
     """
-    # Step 1: Prepare graph and initialize variables
-    graph = preprocess_graph(graph)  # Ensure weights are present and relabel nodes
-    n_nodes = len(graph.nodes())  # Number of nodes in the graph
-    w, wtot, s = initialize_weights(graph, n_nodes)  # Initialize node weights and cluster sizes
-    
-    dendrogram = []  # Store the hierarchical clustering steps
-    cc = []  # List to hold connected components
-    next_cluster_id = n_nodes  # ID for new clusters formed during the process
+    # Preprocess the graph and initialize variables:
+    # - `graph_copy`: A copy of the input graph to avoid modifying the original.
+    # - `n_nodes`: Total number of nodes in the graph.
+    # - `s`: A dictionary storing the size of each cluster (number of nodes in each cluster).
+    # - `w`: A dictionary storing weights for each node.
+    # - `wtot`: The total weight of the graph.
+    graph_copy, n_nodes, s, w, wtot = process_graph(graph)
 
-    # Step 2: Perform hierarchical clustering
+    # Initialize a list to store connected components and the dendrogram
+    cc = []  # Stores nodes that cannot be merged (connected components)
+    dendrogram = []  # Stores the hierarchical clustering result
+
+    # Start a unique node index for new clusters
+    u = n_nodes
+
+    # Main loop: Continue until all nodes are processed
     while n_nodes > 0:
-        # Start with a chain of nodes for nearest-neighbor clustering
-        chain = [list(graph.nodes())[0]]
-        while chain:
-            a = chain.pop()  # Take the last node in the chain
-            b, d = find_closest_neighbor(graph, a, w, wtot)  # Find closest neighbor
-
-            if chain:  # If the chain is not empty
+        # Initialize the "chain" with the first node in the graph
+        chain = [list(graph_copy.nodes())[0]]
+        # Process the chain until it is empty
+        while chain != []:
+            # Pop the last node in the chain
+            a = chain.pop()
+            # Find the closest neighbor (node `b`) and the distance `d`
+            b, d = get_closest_neighbor(a, graph_copy, w, wtot)
+            # Check if the chain is not empty
+            if chain != []:
+                # Get the second-to-last node in the chain
                 c = chain.pop()
-                if b == c:  # Nodes `a` and `b` can merge
-                    dendrogram.append([a, b, d, s[a] + s[b]])  # Record the merge step
-                    merge_clusters(graph, a, b, next_cluster_id, w, s)  # Merge clusters
-                    next_cluster_id += 1  # Increment cluster ID
-                    n_nodes -= 1  # Decrease the node count
-                else:  # If no merge happens, extend the chain
-                    chain.extend([c, a, b])
-            elif b >= 0:  # If a neighbor is found, add it to the chain
-                chain.extend([a, b])
-            else:  # If no neighbor, isolate the node
+                # If the closest neighbor `b` matches `c`, merge nodes `a` and `b`
+                if b == c:
+                    # Add the merge operation to the dendrogram
+                    dendrogram.append([a, b, d, s[a] + s[b]])
+                    # Merge nodes `a` and `b` into a new cluster and update the graph
+                    n_nodes = ad_new_cluster(a, b, graph_copy, n_nodes, s, u, w)
+                    # Increment the unique cluster index
+                    u += 1
+                else:
+                    # If `b` does not match `c`, push `c`, `a`, and `b` back onto the chain
+                    chain.append(c)
+                    chain.append(a)
+                    chain.append(b)
+            elif b >= 0:
+                # If chain is empty but a valid neighbor `b` exists, add `a` and `b` back to the chain
+                chain.append(a)
+                chain.append(b)
+            else:
+                # If no neighbor exists, add node `a` to the connected components
                 cc.append((a, s[a]))
-                graph.remove_node(a)  # Remove isolated node
-                w.pop(a)  # Update weights
-                s.pop(a)  # Update sizes
-                n_nodes -= 1
+                # Remove node `a` from the graph
+                n_nodes = remove_node(a, graph_copy, n_nodes, s, w)
 
-    # Step 3: Handle any remaining connected components
-    process_connected_components(cc, dendrogram, next_cluster_id)
+    # Handle remaining connected components in `cc` (node that are still alone)
+    process_remaining_components(cc, dendrogram, u)
 
-    # Step 4: Reorder the dendrogram rows for standard hierarchical clustering format
+    # Reorder the dendrogram and return it as a numpy array
     return reorder_dendrogram(np.array(dendrogram))
 
 
-def preprocess_graph(graph):
+def process_remaining_components(cc, dendrogram, u):
+    # Start with the first remaining node
+    a, s = cc.pop()
+    for b, t in cc:
+        # Update the cluster size `s` and add the merge operation to the dendrogram
+        s += t
+        dendrogram.append([a, b, float("inf"), s])
+        # Increment the unique cluster index
+        a = u
+        u += 1
+
+
+def remove_node(a, graph_copy, n_nodes, s, w):
+    graph_copy.remove_node(a)
+    w.pop(a)
+    s.pop(a)
+    n_nodes -= 1
+    return n_nodes
+
+
+def ad_new_cluster(a, b, graph_copy, n_nodes, s, u, w):
     """
-    Ensure the graph has weighted edges and relabel nodes for easier processing.
-
-    Parameters
-    ----------
-    graph : networkx.Graph
-        Input graph.
-
-    Returns
-    -------
-    networkx.Graph
-        Processed graph with weights assigned to all edges.
+    Merge two nodes (a and b) into a new cluster (u) and update the graph.
     """
-    graph = nx.convert_node_labels_to_integers(graph.copy())  # Relabel nodes as integers
-    if not nx.get_edge_attributes(graph, 'weight'):  # If weights are missing
-        nx.set_edge_attributes(graph, 1, 'weight')  # Assign default weight of 1
-    return graph
+    # Add a new node `u` to represent the merged cluster
+    graph_copy.add_node(u)
+
+    # Get the neighbors of node `a` and node `b`
+    neighbors_a = list(graph_copy.neighbors(a))
+    neighbors_b = list(graph_copy.neighbors(b))
+
+    # Add edges from the new cluster `u` to the neighbors of `a`
+    for v in neighbors_a:
+        graph_copy.add_edge(u, v, weight=graph_copy[a][v]['weight'])
+
+    # Add edges from the new cluster `u` to the neighbors of `b`
+    for v in neighbors_b:
+        if graph_copy.has_edge(u, v):
+            # If an edge already exists between `u` and `v`, combine the weights
+            graph_copy[u][v]['weight'] += graph_copy[b][v]['weight']
+        else:
+            # Otherwise, add a new edge between `u` and `v`
+            graph_copy.add_edge(u, v, weight=graph_copy[b][v]['weight'])
+
+    # Remove the original nodes `a` and `b` from the graph
+    graph_copy.remove_node(a)
+    graph_copy.remove_node(b)
+
+    # Decrease the total number of nodes
+    n_nodes -= 1
+
+    # Update the weight of the new cluster `u` as the sum of weights of `a` and `b`
+    w[u] = w.pop(a) + w.pop(b)
+
+    # Update the size of the new cluster `u` as the sum of sizes of `a` and `b`
+    s[u] = s.pop(a) + s.pop(b)
+
+    # Return the updated number of nodes
+    return n_nodes
 
 
-def initialize_weights(graph, n_nodes):
-    """
-    Initialize node weights, total weight of the graph, and cluster sizes.
 
-    Parameters
-    ----------
-    graph : networkx.Graph
-        Input graph.
-    n_nodes : int
-        Number of nodes in the graph.
+def get_closest_neighbor(a, graph_copy, w, wtot):
+    d_min = float("inf")
+    b = -1
+    neighbors_a = list(graph_copy.neighbors(a))
+    for v in neighbors_a:
+        if v != a:
+            d = w[v] * w[a] / float(graph_copy[a][v]['weight']) / float(wtot)
+            if d < d_min:
+                b = v
+                d_min = d
+            elif d == d_min:
+                b = min(b, v)
+    d = d_min
+    return b, d
 
-    Returns
-    -------
-    tuple
-        (weights, total_weight, sizes), where:
-        - weights: dict of node weights
-        - total_weight: sum of all edge weights
-        - sizes: dict of cluster sizes
-    """
-    w = {u: 0 for u in range(n_nodes)}  # Node weights
-    s = {u: 1 for u in range(n_nodes)}  # Cluster sizes
-    wtot = 0  # Total weight of the graph
 
-    # Calculate weights and total weight
-    for u, v, data in graph.edges(data=True):
-        weight = data['weight']
+def process_graph(graph):
+    graph_copy = graph.copy()
+    nodes = list(graph_copy.nodes())
+    n_nodes = len(nodes)
+    graph_copy = nx.convert_node_labels_to_integers(graph_copy)
+    if nx.get_edge_attributes(graph_copy, 'weight') == {}:
+        for u, v in graph_copy.edges():
+            graph_copy.add_edge(u, v, weight=1)
+    w = {u: 0 for u in range(n_nodes)}
+    wtot = 0
+    for (u, v) in graph_copy.edges():
+        weight = graph_copy[u][v]['weight']
         w[u] += weight
         w[v] += weight
-        wtot += 2 * weight  # Each edge contributes to both nodes
-
-    return w, wtot, s
-
-
-def find_closest_neighbor(graph, node, weights, total_weight):
-    """
-    Find the closest neighbor to the given node based on the Paris distance.
-
-    Parameters
-    ----------
-    graph : networkx.Graph
-        Input graph.
-    node : int
-        Current node.
-    weights : dict
-        Weights of nodes.
-    total_weight : float
-        Total weight of all edges in the graph.
-
-    Returns
-    -------
-    tuple
-        (closest_node, min_distance)
-    """
-    min_distance = float("inf")
-    closest_node = -1
-
-    # Iterate through neighbors to find the closest one
-    for neighbor in graph.neighbors(node):
-        if neighbor != node:
-            distance = (weights[node] * weights[neighbor]) / graph[node][neighbor]['weight'] / total_weight
-            if distance < min_distance or (distance == min_distance and neighbor < closest_node):
-                closest_node, min_distance = neighbor, distance
-
-    return closest_node, min_distance
-
-
-def merge_clusters(graph, a, b, new_node, weights, sizes):
-    """
-    Merge two clusters into a new cluster.
-
-    Parameters
-    ----------
-    graph : networkx.Graph
-        Input graph.
-    a, b : int
-        Nodes to be merged.
-    new_node : int
-        ID for the new cluster node.
-    weights : dict
-        Node weights.
-    sizes : dict
-        Cluster sizes.
-    """
-    graph.add_node(new_node)  # Add new cluster node
-
-    # Transfer edges from old nodes to the new cluster
-    for neighbor in set(graph.neighbors(a)).union(graph.neighbors(b)):
-        weight = (graph[a][neighbor]['weight'] if graph.has_edge(a, neighbor) else 0) + \
-                 (graph[b][neighbor]['weight'] if graph.has_edge(b, neighbor) else 0)
-        graph.add_edge(new_node, neighbor, weight=weight)
-
-    graph.remove_nodes_from([a, b])  # Remove merged nodes
-    weights[new_node] = weights.pop(a) + weights.pop(b)  # Update weights
-    sizes[new_node] = sizes.pop(a) + sizes.pop(b)  # Update sizes
-
-
-def process_connected_components(components, dendrogram, next_cluster_id):
-    """
-    Process remaining connected components and add them to the dendrogram.
-
-    Parameters
-    ----------
-    components : list
-        Remaining connected components.
-    dendrogram : list
-        Current dendrogram.
-    next_cluster_id : int
-        Next cluster ID for new clusters.
-    """
-    a, size = components.pop()
-    for b, t in components:
-        size += t
-        dendrogram.append([a, b, float("inf"), size])
-        a = next_cluster_id
-        next_cluster_id += 1
+        wtot += 2 * weight
+    s = {u: 1 for u in range(n_nodes)}
+    return graph_copy, n_nodes, s, w, wtot
 
 
 def reorder_dendrogram(dendrogram):
     """
-    Reorder dendrogram rows by increasing distances.
+     Given a graph, compute the paris hierarchy
 
-    Parameters
-    ----------
-    dendrogram : numpy.array
-        Unsorted dendrogram.
+     Parameters
+     ----------
+     dendrogram: numpy.array
+         Each line of the dendrogram contains the merged nodes, the distance between merged nodes and the number of
+         nodes in the new cluster. The lines are not sorted with respect to increasing distances.
 
-    Returns
-    -------
-    numpy.array
-        Sorted dendrogram by increasing distances.
-    """
-    n = len(dendrogram) + 1
-    order = np.lexsort((dendrogram[:, 2], np.arange(len(dendrogram))))  # Sort by distance, then by index
-    mapping = {i: i for i in range(n)}  # Initial mapping of indices
-    mapping.update({n + order[i]: n + i for i in range(len(dendrogram))})
+     Returns
+     -------
+     dendrogram: numpy.array
+         Each line of the dendrogram contains the merged nodes, the distance between merged nodes and the number of
+         nodes in the new cluster. The lines are sorted with respect to increasing distances.
 
-    # Reorder the dendrogram based on sorted indices
-    return np.array([
-        [mapping[int(row[0])], mapping[int(row[1])], row[2], row[3]]
-        for row in dendrogram[order]
-    ])
+     References
+     ----------
+     -
+     """
+    n = np.shape(dendrogram)[0] + 1
+    order = np.zeros((2, n - 1), float)
+    order[0] = range(n - 1)
+    order[1] = np.array(dendrogram)[:, 2]
+    index = np.lexsort(order)
+    n_index = {i: i for i in range(n)}
+    n_index.update({n + index[t]: n + t for t in range(n - 1)})
+    return np.array([[n_index[int(dendrogram[t][0])], n_index[int(dendrogram[t][1])], dendrogram[t][2], dendrogram[t][3]] for t in range(n - 1)])[index, :]
